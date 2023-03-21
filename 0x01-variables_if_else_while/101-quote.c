2b1702b812fe57353e21029422b2041096590204
Judith-Obiesie <nkechinyeremkamma@gmail.com>
2023-03-20 17:41:52 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unisted.h>
+/**
+ * main - A C program that prints a line to the standrad error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\",
+			- Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
